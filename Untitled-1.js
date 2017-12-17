42358b563b03d12a674c73f7783bac06d8ea51f7
Zack Toth <zacharyrtoth@gmail.com>
2017-12-17 10:28:14 -0800

    removed untitled.js

--- Untitled-1.js
+++
@@ -1,9 +0,0 @@
-$(".submit").click((e) => {
-  e.preventDefault();
-  let searchTerm = $(".text").val();
-  $.ajax({
-  url: `https://jobs.github.com/positions.json?q=${searchTerm}`
-}).done(function(data) {
-  $('.results').html(`${data}`);
-  })
-});
